93f57f425f0032ae84316129156c4b90ad3fc1e3
Rishav <51906186+rishavmahapatra@users.noreply.github.com>
2023-09-10 23:13:48 +0530

    Create  Inverted Numeric Pyramid.java
    Take an integer N as input, print the corresponding Numeric Inverted Half Pyramid pattern for N.
    
    For example if N = 4 then pattern will be like:
    
    1 2 3 4
    1 2 3
    1 2
    1

---
+++ Patterns/Inverted Numeric Pyramid.java
@@ -0,0 +1,20 @@
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUTS
+        // E.g. 'Scanner' for input & 'System.out' for output
+        Scanner sc = new Scanner(System.in);
+        int N= sc.nextInt();
+        for(int r=1; r<=N; r++){
+            for(int c=1; c<=N+1-r; c++){
+                if (c==N+1-r) System.out.print(c);
+               else System.out.print(c + " ");
+            }
+            System.out.println();
+        }
+    }
+}
